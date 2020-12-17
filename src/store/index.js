846b75441fb4b69c6c8b9c3c859167f6924093ef
mellkior <mlmandych@roadrunner.com>
2020-12-16 21:54:22 -0500

    Added simplest possible api consumer
    Added store action and mutation and boards view

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    boards: []
   },
   mutations: {
+    setBoards(state, data) {
+      state.boards = data;
+      console.log(data);
+    }
   },
   actions: {
+    async loadBoardsAsync(context) {
+      const { data } = await axios.get('http://127.0.0.1:8000/boards/', {
+        headers: {
+          Authorization: 'Token 4e4898303f4ead5cb3d400d66630cfb747457938'
+        }
+      });
+      context.commit("setBoards", data);
+    }
   },
   modules: {
   }
---
+++ src/views/Boards.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="boards">
+    <table id="board-table">
+        <tr>
+            <th>Name</th>
+            <th>Archived</th>
+            <th>Position</th>
+        </tr>
+        <tr v-for="board in boards" v-bind:key="board.url">
+            <td>{{ board.name }}</td>
+            <td>{{ board.archived }}</td>
+            <td>{{ board.position }}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import store from '@/store'
+
+export default {
+  name: 'Boards',
+  components: {},
+  computed: {
+    boards () {
+      return store.state.boards;
+    }
+  },
+  created: function () {
+      console.log("Loading Boards...");
+      store.dispatch('loadBoardsAsync');
+      console.log("Done loading Boards");
+  }
+}
+</script>
