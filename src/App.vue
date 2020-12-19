89c89694ee56eab9ab14e3fa2ddc92b281d0b06d
mellkior <mlmandych@roadrunner.com>
2020-12-18 20:21:31 -0500

    Added dark style

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,19 @@
 </template>
 
 <style>
+/* global style */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  margin-top: 60px;
+  color: #10cc39;
+}
+
+body {
+  color: #10cc39;
+  background-color: #33333f;
 }
 
 #nav {
@@ -22,10 +29,10 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #45878a;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #10cc39;
 }
 </style>
