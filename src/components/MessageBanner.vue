14293c30e05bcfd5ea1fec2e187ed55c5d467003
mellkior <mlmandych@roadrunner.com>
2021-04-04 14:27:31 -0400

    Made messages disappear on a timer

--- src/components/MessageBanner.vue
+++ src/components/MessageBanner.vue
@@ -5,70 +5,7 @@
       :key="message"
       class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8"
     >
-      <div class="flex items-center justify-between flex-wrap">
-        <div class="w-0 flex-1 flex items-center">
-          <span class="flex p-2 rounded-lg bg-indigo-800">
-            <!-- Heroicon name: outline/speakerphone -->
-            <svg
-              class="h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
-              />
-            </svg>
-          </span>
-          <p class="ml-3 font-medium text-white truncate">
-            <span class="hidden md:inline">
-              {{ message }}
-            </span>
-          </p>
-        </div>
-        <!-- Learn More Button -->
-        <!--<div
-          class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto"
-        >
-          <a
-            href="#"
-            class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
-          >
-            More Info
-          </a>
-        </div>-->
-
-        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-          <button
-            @click="remove(message)"
-            type="button"
-            class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-          >
-            <span class="sr-only">Dismiss</span>
-            <!-- Heroicon name: outline/x -->
-            <svg
-              class="h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+      <Message v-bind:messageProp="message" v-bind:secondsProp="4" v-on:destroy="remove(message)"/>
     </div>
     <div class="flex justify-center font-small">
       <button v-if="messages.length > 0" @click="clear()">
@@ -80,6 +17,7 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex';
+import Message from '@/components/Message.vue';
 
 export default {
   name: 'MessageBanner',
@@ -88,6 +26,9 @@ export default {
   },
   methods: {
     ...mapMutations({ clear: 'clearMessages', remove: 'removeMessage', pop: 'popMessage'}),
+  },
+  components: {
+      Message,
   },
 };
 </script>
